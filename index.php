e13f8ca260e46411077ace042f9ff7332bca3454
Houtman98 <62240193+Houtman98@users.noreply.github.com>
2020-04-01 14:40:45 +0700

    Add files via upload

---
+++ index.php
@@ -0,0 +1,80 @@
+<html>
+<head>
+	<script src="js/jquery.js"></script>
+	<meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Statistik COVID-19 Indonesia</title>
+    <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
+    <link rel="stylesheet" href="css/css/materialize.min.css">
+    <link rel="stylesheet" href="css/style.css">
+    <script src="js/jquery.js"></script>
+    <script src="css/js/materialize.min.js"></script>
+	<link rel="shortcut icon" href="img/favicon.ico">
+</head>
+<body>
+	
+			
+          
+              
+	
+		<p style="color:black; background-color:#BAB4B3"><marquee> Stay Home</marquee></p>			
+	
+	
+
+			<!-- Start  Contain index -->
+    <div class="container">
+        <h3 class="center">Statistik Covid-19 Indonesia</h3>
+        </br></br></br>
+        <div class="row">
+                <div class="col s12 m4">
+                    <div class="card-panel">
+						<div align="center">
+							<p style="color:black; background-color:#f7be16"><b>TOTAL POSITIF</b></p>			
+                            <h5 class="center"><b><span id='terjangkit'></b></span></h5>	
+						</div>
+                    </div>
+                </div>
+                <div class="col s12 m4">
+                    <div class="card-panel">                         
+						<div align="center">	
+							<p style="color:white; background-color:#36622b"><b>TOTAL SEMBUH</b></p>
+                            <h5 class="center"><b><span id='sembuh'></b></span></h5>                          
+                        </div>
+                    </div>
+                </div>
+                <div class="col s12 m4">
+                    <div class="card-panel">
+                        <div align="center">        
+							<p style="color:white; background-color:#F94846"><b>TOTAL MENINGGAL</b></p>
+                            <h5 class="center"><b><span id='meninggal'></span></b></h5>
+						</div>
+				</div>
+            </div>
+    </div>
+     <!-- Close  Contain index -->
+
+<h6 class="center">Sumber data : Kementerian Kesehatan & JHU. Update terakhir : <span id="date"></span></h6>
+
+	
+	 
+	 <br><br><br><br><br><br><br><br><br>
+	   <!-- Start Footer -->
+	<footer>
+		<div class="center">
+			<div class="row">
+				<div class="page-copyright">
+				
+					<p class="black-text">AlfiyanN</a> . 
+					Data Live By &copy; <a href="https://kawalcorona.com/"> Ethical Hacker Indonesia</a>. </p>
+				</div>
+			</div>
+		</div>
+	</footer>
+	<!--End Footer-->
+	 
+	<script src="js/script.js"></script>
+
+</body>
+</html>
+
